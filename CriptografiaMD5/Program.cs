021b64d2f524e01b1189089abcd08f69bb94d177
hvitorino <hamon.vitorino@gmail.com>
2013-04-22 10:51:13 -0300

    Vai tartaruguinha

---
+++ CriptografiaMD5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CriptografiaMD5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Digite a senha a criptografar: ");
+            var senha = Console.ReadLine();
+            Console.Write("Senha a criptografada        : ");
+            Console.WriteLine(CriptografaSenha(senha));
+
+            Console.ReadLine();
+        }
+
+        static string CriptografaSenha(string senha)
+        {
+            var md5 = MD5.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(senha);
+            var hash = md5.ComputeHash(inputBytes);
+
+            return BitConverter.ToString(hash);
+        }
+    }
+}
